79085e02f5cb004cd5406ffc43458ca0a843ad8d
thiago juvenal <thiagoth4@hotmail.com>
2020-04-13 08:11:09 -0300

    addd principal

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, result;
+            Console.WriteLine("Informe um numero para a tabuada. ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <=10; i++)
+            {
+                result = num * i;
+                Console.WriteLine(num+" x " + i +" = "+result);
+            }
+        }
+    }
+}
